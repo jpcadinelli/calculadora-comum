78716e079636ef81ca7c9dac8deb9a0b1f84f955
Joao Pedro Cadinelli <jpcadinelli@gmail.com>
2023-09-20 15:35:58 -0300

    arrumado

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 
-const screen = document.querySelector('.visor');
+const screen = document.querySelector(".visor");
 
-function buttonClick(value) {
-    if(isNaN(value)) {
+function buttonClick(value){
+    if(isNaN(value)){
         handleSymbol(value);
     } else {
-        handleSymbol(value);
+        handleNumber(value);
     }
     screen.innerText = buffer;
 }
 
-function handleSymbol(symbol) {
-    switch(symbol) {
+function handleSymbol(symbol){
+    switch(symbol){
         case 'C':
-            buffer = '0';
+            buffer = 0;
             runningTotal = 0;
             break;
         case '=':
-            if (previousOperator === null) {
+            if(previousOperator === null){
                 return
             }
             flushOperation(parseInt(buffer));
@@ -29,10 +29,10 @@ function handleSymbol(symbol) {
             runningTotal = 0;
             break;
         case '←':
-            if (buffer.length === 1) {
+            if(buffer.length === 1){
                 buffer = '0';
             } else {
-                buffer = buffer.toString(0, buffer.length - 1);
+                buffer = buffer.substring(0, buffer.length - 1); 
             }
             break;
         case '+':
@@ -44,14 +44,13 @@ function handleSymbol(symbol) {
     }
 }
 
-function handleMath(symbol) {
-    if (buffer === '0') {
+function handleMath(symbol){
+    if(buffer === "0"){
         return;
     }
-
     const intBuffer = parseInt(buffer);
 
-    if (runningTotal === 0) {
+    if(runningTotal === 0){
         runningTotal = intBuffer;
     } else {
         flushOperation(intBuffer);
@@ -60,8 +59,8 @@ function handleMath(symbol) {
     buffer = '0';
 }
 
-function flushOperation(intBuffer) {
-    if (previousOperator === '+') {
+function flushOperation(intBuffer){
+    if(previousOperator === '+'){
         runningTotal += intBuffer;
     } else if (previousOperator === '−') {
         runningTotal -= intBuffer;
@@ -71,19 +70,21 @@ function flushOperation(intBuffer) {
         runningTotal /= intBuffer;
     }
 }
+    
 
-function handleNumber(numberString) {
-    if (buffer === '0') {
+function handleNumber(numberString){
+    if(buffer === '0') {
         buffer = numberString;
     } else {
-        buffer += numberString
+        buffer += numberString;
     }
 }
 
-function init() {
-    document.querySelector('.botoes').addEventListener('click', function(event){
+function init2(){
+    document.querySelector('.botoes').
+    addEventListener('click', function(event){
         buttonClick(event.target.innerText);
     })
 }
 
-init();
+init2();
